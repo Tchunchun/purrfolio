f8a6a716269564ecfd694a6873da1ed2aa2cc770
Tchunchun <chunchun2033@gmail.com>
2016-01-06 14:39:19 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -20,7 +20,5 @@ $email_body = "You have received a new message from your website contact form.\n
 $headers = "From: noreply@irenezhu.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-//for testing
-mail("chunchun.tong@aol.com","testing2","testing body","testing Header");
 return true;
 ?>
